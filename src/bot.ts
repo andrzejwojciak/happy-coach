4f9118e5dbb94940624d8452a2a61450e4071803
andrzejwojciak <gombkaa@gmail.com>
2022-12-29 20:37:40 +0100

    Add help command

--- src/bot.ts
+++ src/bot.ts
@@ -36,6 +36,7 @@ app.message(
   new RegExp(/^stats (distance|time) [2-9][0-9]{3}$/),
   async ({ message, say }) => {
     const mess = message as Message;
+    if (mess.thread_ts) return;
     const text = mess.text;
     const year = text.match(/[2-9][0-9]{3}/);
     const activity = text.match(/(distance|time)/);
@@ -51,11 +52,60 @@ app.message(
 );
 
 app.message(new RegExp(/^stats$/), async ({ message, say }) => {
+  const mess = message as Message;
+  if (mess.thread_ts) return;
   const timeValues = await recordService.getCurrentValues('time');
   const distanceValue = await recordService.getCurrentValues('distance');
 
   await say(`Time: ${timeValues}, distance: ${distanceValue}`);
 });
+
+app.message(new RegExp(/^help$/), async ({ message, say }) => {
+  const mess = message as Message;
+  if (mess.thread_ts) return;
+
+  let commands = '• help - you have just used it\n';
+  commands +=
+    '• +{value}{h|km|min} - will add and calculate the values. Can be placed multiple times anywhere in the message, can also be used in threads (eg. +10km)\n';
+  commands +=
+    '• stats - will display the distance and time over all time, can be used in the main chat\n';
+  commands +=
+    '• stats (distance|time) {year} - will display the distance or time for a given year, can be used in the main chat (eg. stats distance 2022)\n';
+  commands +=
+    '• HappyCoach (-v|version) - will display version of the application, can be used in main chat.\n';
+  commands +=
+    '• HappyCoach changelog - will display changes to the application version, and can be used in the main chat.\n';
+
+  say({
+    text: commands,
+  });
+});
+
+app.message(
+  new RegExp(/^(happycoach|HappyCoach|Coach) (version|-v)$/),
+  async ({ message, say }) => {
+    const mess = message as Message;
+    if (mess.thread_ts) return;
+
+    const version = '*HappyCoach®* v1.0.2\n\nWith <3 from Legnica';
+    say({ text: version });
+  }
+);
+
+app.message(
+  new RegExp(/^(happycoach|HappyCoach|Coach) changelog$/),
+  async ({ message, say }) => {
+    const mess = message as Message;
+    if (mess.thread_ts) return;
+
+    let changes: string;
+    changes =
+      '*v1.0.2 (29.12.2022)*: \n\n• Added commands: changelog, version, help \n';
+    changes +=
+      "\n*v1.0.1 (28.12.2022)*: \n\n• Added handling for unit 'm' in the add value command \n• Fixed NaN issue while adding value with ',' \n• Added replies for threads ";
+    say({ text: changes });
+  }
+);
 
 app.message(
   new RegExp('\\+[0-9][0-9]{0,2}(?:[.][0-9]{0,2})?m(?!i)', 'm'),
